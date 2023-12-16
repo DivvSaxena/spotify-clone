d55bb884c0246c2bc1d256724fa6cf22c414bb9f
Divv Saxena <saxenadivv@gmail.com>
2023-12-16 10:57:06 +0530

    Add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,8 @@ const Card = (prop) => {
   return (
     <>
       <div className='container-card'>
-        <img src={prop.item.imgUrl} className='container-card-img'/>
+      
+        <img src= {prop.item.imgUrl} className='container-card-img'/>
         <div className='container-text'>
           <h3>{prop.item.title}</h3>
           <p>{prop.item.description}</p>
--- src/data.js
+++ src/data.js
@@ -1,32 +1,34 @@
+import img from "./assets/img-three.jpg";
+
 export default [
   {
-    imgUrl: "./assets/img-four.jpg",
+    imgUrl: "src/assets/img-four.jpg",
     title: "Today's Top Hits",
-    description: "Tate McRae is on top of the Hottest 50!"
+    description: "Tate McRae is on top of the Hottest 50!",
   },
   {
-    imgUrl: "./assets/img-three.jpg",
+    imgUrl: "src/assets/img-three.jpg",
     title: "Chillhop",
-    description:"Blessed out beats and head nodding groov..."
+    description: "Blessed out beats and head nodding groov...",
   },
   {
-    imgUrl: "./assets/img-two.jpg",
+    imgUrl: "src/assets/img-two.jpg",
     title: "Christmas Hits",
-    description:"The biggest Christmas songs of all time."
+    description: "The biggest Christmas songs of all time.",
   },
   {
-    imgUrl: "./assets/img-one.jpg",
+    imgUrl: "src/assets/img-one.jpg",
     title: "Calming Acoustic",
-    description:"Keep calm with acoustic tracks."
+    description: "Keep calm with acoustic tracks.",
   },
   {
-    imgUrl: "./assets/img-six.jpg",
+    imgUrl: "src/assets/img-six.jpg",
     title: "Sleep",
-    description: "Gentle Ambient piano to help you fall asleep."
-  }
+    description: "Gentle Ambient piano to help you fall asleep.",
+  },
   // {
-  //   imgUrl: "./assets/img-five.jpg",
+  //   imgUrl: "src/assets/img-five.jpg",
   //   title: "Night Rain",
   //   description: "Sleep with sounds of pouring rain and..."
   // }
-]
+];
