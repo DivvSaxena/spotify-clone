71906d08265d9f3e8d285793f83424c5165a6121
Divv Saxena <saxenadivv@gmail.com>
2023-12-21 01:13:44 +0530

    add

--- src/data.js
+++ src/data.js
@@ -1,38 +1,37 @@
-import img from "./assets/img-three.jpg";
 
 export default [
   {
-    imgUrl: "src/assets/img-four.jpg",
+    imgUrl: "/assets/img-four.jpg",
     title: "Today's Top Hits",
     description: "Tate McRae is on top of the Hottest 50!",
-    songUrl: "src/assets/music/song1.mp3"
+    songUrl: "/assets/music/song1.mp3"
   },
   {
-    imgUrl: "src/assets/img-three.jpg",
+    imgUrl: "/assets/img-three.jpg",
     title: "Chillhop",
     description: "Blessed out beats and head nodding groov...",
-    songUrl: "src/assets/music/song2.mp3"
+    songUrl: "/assets/music/song2.mp3"
   },
   {
-    imgUrl: "src/assets/img-two.jpg",
+    imgUrl: "/assets/img-two.jpg",
     title: "Christmas Hits",
     description: "The biggest Christmas songs of all time.",
-    songUrl: "src/assets/music/song3.mp3"
+    songUrl: "/assets/music/song3.mp3"
   },
   {
-    imgUrl: "src/assets/img-one.jpg",
+    imgUrl: "/assets/img-one.jpg",
     title: "Calming Acoustic",
     description: "Keep calm with acoustic tracks.",
-    songUrl: "src/assets/music/song4.mp3"
+    songUrl: "/assets/music/song4.mp3"
   },
   {
-    imgUrl: "src/assets/img-six.jpg",
+    imgUrl: "/assets/img-six.jpg",
     title: "Sleep",
     description: "Gentle Ambient piano to help you fall asleep.",
-    songUrl: "src/assets/music/song5.mp3"
+    songUrl: "/assets/music/song5.mp3"
   }
   // {
-  //   imgUrl: "src/assets/img-five.jpg",
+  //   imgUrl: "/assets/img-five.jpg",
   //   title: "Night Rain",
   //   description: "Sleep with sounds of pouring rain and..."
   // }
